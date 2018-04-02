382aa7c052e267049b164ae8f8d0a6d76ea560a6
Christopher Goes <cwgoes@protonmail.ch>
2018-04-02 15:48:10 +0200

    Implement InitGenesis for x/stake (closes #737)

--- x/stake/keeper.go
+++ x/stake/keeper.go
@@ -1,6 +1,9 @@
 package stake
 
 import (
+	"encoding/json"
+	"errors"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/wire"
 	"github.com/cosmos/cosmos-sdk/x/bank"
@@ -24,6 +27,17 @@ func NewKeeper(ctx sdk.Context, cdc *wire.Codec, key sdk.StoreKey, ck bank.CoinK
 		coinKeeper: ck,
 	}
 	return keeper
+}
+
+// InitGenesis - store genesis parameters
+func (k Keeper) InitGenesis(ctx sdk.Context, data json.RawMessage) error {
+	var state GenesisState
+	if err := json.Unmarshal(data, &state); err != nil {
+		return err
+	}
+	k.setPool(ctx, state.Pool)
+	k.setParams(ctx, state.Params)
+	return nil
 }
 
 //_________________________________________________________________________
@@ -279,8 +293,7 @@ func (k Keeper) GetParams(ctx sdk.Context) (params Params) {
 	store := ctx.KVStore(k.storeKey)
 	b := store.Get(ParamKey)
 	if b == nil {
-		k.params = defaultParams()
-		return k.params
+		panic(errors.New("Stored params should not have been nil"))
 	}
 
 	err := k.cdc.UnmarshalBinary(b, &params)
@@ -310,7 +323,7 @@ func (k Keeper) GetPool(ctx sdk.Context) (gs Pool) {
 	store := ctx.KVStore(k.storeKey)
 	b := store.Get(PoolKey)
 	if b == nil {
-		return initialPool()
+		panic(errors.New("Stored pool should not have been nil"))
 	}
 	err := k.cdc.UnmarshalBinary(b, &gs)
 	if err != nil {
